f5f1f7cd36b82022db779c60639f347ddd59fa2c
FFF <nattapol.tongsom@kingpower.com>
2025-08-07 19:07:47 +0700

    push

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/bbymedia-web', 
-  assetPrefix: '/bbymedia-web/', 
+  basePath: '/bbymedia-web',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
