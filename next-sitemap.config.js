8adb1292e0363a432b31af6114d1f91180f6fa2d
FFF <nattapol.tongsom@kingpower.com>
2025-08-11 16:04:17 +0700

    seo

---
+++ next-sitemap.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  siteUrl: 'https://bbymedia.com',
+  generateRobotsTxt: true,
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import GalleryPros from "@/components/GalleryProps";
 import Hero from "@/components/Hero";
 import HeroSlider from "@/components/HeroSlider";
 import Services from "@/components/Services";
+import Head from "next/head";
 
 export default function HomePage() {
   const portfolioImages = [
@@ -13,16 +14,21 @@ export default function HomePage() {
   ];
 
   return (
-    <div className="bg-gray-50 text-gray-900 font-sans">
-      <HeroSlider />
-      <Hero />
-      <Services />
-      <GalleryPros
-        images={portfolioImages}
-        title="ผลงานของเรา"
-      />
-      <About />
-      <Contact />
+    <>
+      <Head>
+        <link rel="canonical" href="https://bbymedia.com/" />
+      </Head>
+      <div className="bg-gray-50 text-gray-900 font-sans">
+        <HeroSlider />
+        <Hero />
+        <Services />
+        <GalleryPros
+          images={portfolioImages}
+          title="ผลงานของเรา"
+        />
+        <About />
+        <Contact />
     </div>
+    </>
   );
 }
